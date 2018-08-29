dce07b5ed1c6234fcb155ccd14b7af594554d60f
Orion55 <grol55@mail.ru>
2018-08-29 23:48:45 +0500

    admin page

--- admin/class-cargo-calc-admin.php
+++ admin/class-cargo-calc-admin.php
@@ -20,84 +20,107 @@
  * @subpackage Cargo_Calc/admin
  * @author     Oleg Grebenev <admin@infoblog72.ru>
  */
-class Cargo_Calc_Admin {
+class Cargo_Calc_Admin
+{
 
-	/**
-	 * The ID of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $plugin_name    The ID of this plugin.
-	 */
-	private $plugin_name;
+    /**
+     * The ID of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string $plugin_name The ID of this plugin.
+     */
+    private $plugin_name;
 
-	/**
-	 * The version of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $version    The current version of this plugin.
-	 */
-	private $version;
+    /**
+     * The version of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string $version The current version of this plugin.
+     */
+    private $version;
 
-	/**
-	 * Initialize the class and set its properties.
-	 *
-	 * @since    1.0.0
-	 * @param      string    $plugin_name       The name of this plugin.
-	 * @param      string    $version    The version of this plugin.
-	 */
-	public function __construct( $plugin_name, $version ) {
+    /**
+     * Initialize the class and set its properties.
+     *
+     * @since    1.0.0
+     * @param      string $plugin_name The name of this plugin.
+     * @param      string $version The version of this plugin.
+     */
+    public function __construct($plugin_name, $version)
+    {
 
-		$this->plugin_name = $plugin_name;
-		$this->version = $version;
+        $this->plugin_name = $plugin_name;
+        $this->version = $version;
+        add_action('admin_menu', array($this, 'add_plugin_page'));
+    }
 
-	}
+    /**
+     * Register the stylesheets for the admin area.
+     *
+     * @since    1.0.0
+     */
+    public function enqueue_styles()
+    {
 
-	/**
-	 * Register the stylesheets for the admin area.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_styles() {
+        /**
+         * This function is provided for demonstration purposes only.
+         *
+         * An instance of this class should be passed to the run() function
+         * defined in Cargo_Calc_Loader as all of the hooks are defined
+         * in that particular class.
+         *
+         * The Cargo_Calc_Loader will then create the relationship
+         * between the defined hooks and the functions defined in this
+         * class.
+         */
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Cargo_Calc_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Cargo_Calc_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+        wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . 'css/cargo-calc-admin.css', array(), $this->version, 'all');
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/cargo-calc-admin.css', array(), $this->version, 'all' );
+    }
 
-	}
+    /**
+     * Register the JavaScript for the admin area.
+     *
+     * @since    1.0.0
+     */
+    public function enqueue_scripts()
+    {
 
-	/**
-	 * Register the JavaScript for the admin area.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
+        /**
+         * This function is provided for demonstration purposes only.
+         *
+         * An instance of this class should be passed to the run() function
+         * defined in Cargo_Calc_Loader as all of the hooks are defined
+         * in that particular class.
+         *
+         * The Cargo_Calc_Loader will then create the relationship
+         * between the defined hooks and the functions defined in this
+         * class.
+         */
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Cargo_Calc_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Cargo_Calc_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+        wp_enqueue_script($this->plugin_name, plugin_dir_url(__FILE__) . 'js/cargo-calc-admin.js', array('jquery'), $this->version, false);
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/cargo-calc-admin.js', array( 'jquery' ), $this->version, false );
+    }
 
-	}
+    public function add_plugin_page()
+    {
+        add_options_page(
+            'Cargo calc', // page_title
+            'Калькулятор', // menu_title
+            'manage_options', // capability
+            'cargo-calc', // menu_slug
+            array($this, 'create_admin_page') // function
+        );
+    }
 
+    public function create_admin_page()
+    {
+        ?>
+        <div class="wrap">
+            <h2>Калькулятор стоимости услуг грузового такси</h2>
+            <h3>Шорткод <b>[cargo-calc]</b></h3>
+        </div>
+    <?php }
 }
--- cargo-calc.php
+++ cargo-calc.php
@@ -1,21 +1,15 @@
 <?php
 
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
  *
  * @link              http://portfolio.infoblog72.ru
  * @since             1.0.0
  * @package           Cargo_Calc
  *
  * @wordpress-plugin
- * Plugin Name:       cargo-calc
+ * Plugin Name:       Сargo сalc
  * Plugin URI:        cargo-calc
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Description:       Калькулятор по расчету стоимости услуг грузового такси
  * Version:           1.0.0
  * Author:            Oleg Grebenev
  * Author URI:        http://portfolio.infoblog72.ru
