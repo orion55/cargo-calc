cad34814d24182fd47705fa57297974858894f63
Orion55 <grol55@mail.ru>
2018-09-01 17:42:27 +0500

    select2

---
+++ shortcode/assets/js/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+$(document).ready(function () {
+    $('.calc__dropdown--loaders').select2({
+        width: '180px'
+    });
+
+    function formatState(state) {
+        if (!state.id) {
+            return state.text;
+        }
+        var baseUrl = "assets/img/car";
+        return $('<div class="item-down"><img src="' + baseUrl + '/' + state.element.value.toLowerCase() + '.jpg" class="item-down--img" /><div class="item-down--text">' + state.text + '</div></div>');
+    }
+
+    var selectbox = $('.calc__dropdown--selectbox');
+    selectbox.select2({
+        width: '90%',
+        templateResult: formatState,
+        closeOnSelect: false,
+        selectOnClose: false
+    });
+});
