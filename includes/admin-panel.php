7082c548c3498dcfef04308260f40128c779935a
Orion55 <grol55@mail.ru>
2018-09-17 23:46:10 +0500

    admin fields

--- includes/admin-panel.php
+++ includes/admin-panel.php
@@ -1,4 +1,7 @@
 <?php
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
 
 use Carbon_Fields\Container;
 use Carbon_Fields\Field;
@@ -15,12 +18,42 @@ function crb_attach_theme_options()
                 ->set_help_text('Email для отправки заказов')
         ));
 
-    Container::make('post_meta', __('Опции', 'crb'))
+    Container::make('post_meta', __('Опции заказа', 'crb'))
         ->where('post_type', '=', 'order_cargo')
         ->add_fields(array(
-                Field::make('text', 'price_dish', 'Цена')
-                    ->set_attribute('type', 'number')
-                    ->set_attribute('min', '0')
+                Field::make('text', 'contact_name', 'Имя')
+                    ->set_attribute('type', 'text')
+                    ->set_required(true)
+                    ->set_width(50),
+                Field::make('text', 'contact_phone', 'Телефон')
+                    ->set_attribute('type', 'tel')
+                    ->set_attribute('placeholder', '+7 (***) ***-**-**')
+                    ->set_required(true)
+                    ->set_width(50),
+                Field::make('text', 'address_from', 'Откуда (район)')
+                    ->set_attribute('type', 'text')
+                    ->set_width(40),
+                Field::make('text', 'address_from_street', 'Улица')
+                    ->set_attribute('type', 'text')
+                    ->set_width(40),
+                Field::make('text', 'address_from_house', 'Дом')
+                    ->set_attribute('type', 'text')
+                    ->set_width(10),
+                Field::make('text', 'address_from_entrance', 'Подъезд')
+                    ->set_attribute('type', 'text')
+                    ->set_width(10),
+                Field::make('text', 'address_to', 'Куда (район)')
+                    ->set_attribute('type', 'text')
+                    ->set_width(40),
+                Field::make('text', 'address_to_street', 'Улица')
+                    ->set_attribute('type', 'text')
+                    ->set_width(40),
+                Field::make('text', 'address_to_house', 'Дом')
+                    ->set_attribute('type', 'text')
+                    ->set_width(10),
+                Field::make('text', 'address_to_entrance', 'Подъезд')
+                    ->set_attribute('type', 'text')
+                    ->set_width(10)
             )
         );
 }
